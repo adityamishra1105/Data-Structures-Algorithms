8dff02054a2cdc7af738cecf3c7a72a814027962
Aditya Mishra <130287147+adityamishra1105@users.noreply.github.com>
2024-08-31 12:46:47 +0530

    Add files via upload

---
+++ BubbleSort.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.Scanner;
+public class BubbleSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the Array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Array before BubbleSort: " + Arrays.toString(arr));
+        bubblesort(arr);
+        System.out.println("Array after BubbleSort: " + Arrays.toString(arr));
+        
+    }
+    public static void bubblesort(int[] arr){
+        int n = arr.length;
+        boolean swapped;
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if(arr[j] > arr[j + 1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) break;
+        }
+    }
+}
